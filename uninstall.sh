9f054a44013c51333ac5d890b05d8c70316f4a29
D-W-O-R-D <k.kiriushenkov@gmail.com>
2024-02-06 17:07:59 +0600

    fix style and adding uninstall.sh

---
+++ uninstall.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm -r $PREFIX/glibc/opt/termux-style/mobox-custom
+rm -r $PREFIX/glibc/opt/termux-style/termux-style-custom
+rm -r $PREFIX/glibc/opt/scripts/custom-dynarec-settings
+rm -r $PREFIX/glibc/opt/scripts/custom-mobox
+rm -r $PREFIX/glibc/opt/scripts/custom-settings
+rm -r $PREFIX/glibc/opt/scripts/custom-system-settings
+rm -r $PREFIX/glibc/opt/scripts/mobox_custom_menu
+rm -r $PREFIX/bin/custom-mobox

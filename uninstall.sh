5585d273627e5c718ade92c5542eca2892131a46
D-W-O-R-D <k.kiriushenkov@gmail.com>
2024-02-07 09:34:25 +0600

    Updating uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -7,4 +7,5 @@ rm -r $PREFIX/glibc/opt/scripts/custom-mobox
 rm -r $PREFIX/glibc/opt/scripts/custom-settings
 rm -r $PREFIX/glibc/opt/scripts/custom-system-settings
 rm -r $PREFIX/glibc/opt/scripts/mobox_custom_menu
+rm -r $PREFIX/glibc/opt/scripts/custom-dynarec-presets
 rm -r $PREFIX/bin/custom-mobox

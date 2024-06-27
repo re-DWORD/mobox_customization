b2d3634053d1c8650ed210d59f9eec854071594f
re_DWORD <k.kiriushenkov@gmail.com>
2024-06-27 08:57:16 +0600

    Edited links

--- install.sh
+++ install.sh
@@ -1,25 +1,25 @@
-#!/bin/bash
+R#!/bin/bash
 
-wget -q --retry-connrefused --tries=0 https://raw.githubusercontent.com/D-W-O-R-D/mobox_customization/main/termux-style/mobox-custom/colors.properties -P $PREFIX/glibc/opt/termux-style/mobox-custom
-wget -q --retry-connrefused --tries=0 https://raw.githubusercontent.com/D-W-O-R-D/mobox_customization/main/termux-style/mobox-custom/font.ttf -P $PREFIX/glibc/opt/termux-style/mobox-custom
-wget -q --retry-connrefused --tries=0 https://raw.githubusercontent.com/D-W-O-R-D/mobox_customization/main/termux-style/termux-style-custom -P $PREFIX/glibc/opt/termux-style
+wget -q --retry-connrefused --tries=0 https://raw.githubusercontent.com/re-DWORD/mobox_customization/main/termux-style/mobox-custom/colors.properties -P $PREFIX/glibc/opt/termux-style/mobox-custom
+wget -q --retry-connrefused --tries=0 https://raw.githubusercontent.com/re-DWORD/mobox_customization/main/termux-style/mobox-custom/font.ttf -P $PREFIX/glibc/opt/termux-style/mobox-custom
+wget -q --retry-connrefused --tries=0 https://raw.githubusercontent.com/re-DWORD/mobox_customization/main/termux-style/termux-style-custom -P $PREFIX/glibc/opt/termux-style
 chmod 744 $PREFIX/glibc/opt/termux-style/mobox-custom/colors.properties
 chmod 744 $PREFIX/glibc/opt/termux-style/mobox-custom/font.ttf
 chmod 744 $PREFIX/glibc/opt/termux-style/termux-style-custom
-wget -q --retry-connrefused --tries=0 https://raw.githubusercontent.com/D-W-O-R-D/mobox_customization/main/scripts/custom-dynarec-settings -P $PREFIX/glibc/opt/scripts
-wget -q --retry-connrefused --tries=0 https://raw.githubusercontent.com/D-W-O-R-D/mobox_customization/main/scripts/custom-mobox -P $PREFIX/glibc/opt/scripts
-wget -q --retry-connrefused --tries=0 https://raw.githubusercontent.com/D-W-O-R-D/mobox_customization/main/scripts/custom-settings -P $PREFIX/glibc/opt/scripts
-wget -q --retry-connrefused --tries=0 https://raw.githubusercontent.com/D-W-O-R-D/mobox_customization/main/scripts/custom-system-settings -P $PREFIX/glibc/opt/scripts
-wget -q --retry-connrefused --tries=0 https://raw.githubusercontent.com/D-W-O-R-D/mobox_customization/main/scripts/mobox_custom_menu -P $PREFIX/glibc/opt/scripts
-wget -q --retry-connrefused --tries=0 https://raw.githubusercontent.com/D-W-O-R-D/mobox_customization/main/scripts/custom-dynarec-presets -P $PREFIX/glibc/opt/scripts
+wget -q --retry-connrefused --tries=0 https://raw.githubusercontent.com/re-DWORD/mobox_customization/main/scripts/custom-dynarec-settings -P $PREFIX/glibc/opt/scripts
+wget -q --retry-connrefused --tries=0 https://raw.githubusercontent.com/re-DWORD/mobox_customization/main/scripts/custom-mobox -P $PREFIX/glibc/opt/scripts
+wget -q --retry-connrefused --tries=0 https://raw.githubusercontent.com/re-DWORD/mobox_customization/main/scripts/custom-settings -P $PREFIX/glibc/opt/scripts
+wget -q --retry-connrefused --tries=0 https://raw.githubusercontent.com/re-DWORD/mobox_customization/main/scripts/custom-system-settings -P $PREFIX/glibc/opt/scripts
+wget -q --retry-connrefused --tries=0 https://raw.githubusercontent.com/re-DWORD/mobox_customization/main/scripts/mobox_custom_menu -P $PREFIX/glibc/opt/scripts
+wget -q --retry-connrefused --tries=0 https://raw.githubusercontent.com/re-DWORD/mobox_customization/main/scripts/custom-dynarec-presets -P $PREFIX/glibc/opt/scripts
 chmod 744 $PREFIX/glibc/opt/scripts/custom-dynarec-settings
 chmod 744 $PREFIX/glibc/opt/scripts/custom-mobox
 chmod 744 $PREFIX/glibc/opt/scripts/custom-settings
 chmod 744 $PREFIX/glibc/opt/scripts/custom-system-settings 
 chmod 744 $PREFIX/glibc/opt/scripts/mobox_custom_menu
 chmod 744 $PREFIX/glibc/opt/scripts/custom-dynarec-presets
-wget -q --retry-connrefused --tries=0 https://raw.githubusercontent.com/D-W-O-R-D/mobox_customization/main/uninstall.sh -P $PREFIX/custom-mobox
-wget -q --retry-connrefused --tries=0 https://raw.githubusercontent.com/D-W-O-R-D/mobox_customization/main/custom-mobox-update -P $PREFIX/custom-mobox
+wget -q --retry-connrefused --tries=0 https://raw.githubusercontent.com/re-DWORD/mobox_customization/main/uninstall.sh -P $PREFIX/custom-mobox
+wget -q --retry-connrefused --tries=0 https://raw.githubusercontent.com/re-DWORD/mobox_customization/main/custom-mobox-update -P $PREFIX/custom-mobox
 chmod 744 $PREFIX/custom-mobox/uninstall.sh
 chmod 744 $PREFIX/custom-mobox/custom-mobox-update
 ln -sf $PREFIX/glibc/opt/scripts/custom-mobox $PREFIX/bin/custom-mobox
